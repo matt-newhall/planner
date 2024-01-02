bfedd20d9a54c571b01dc7b2bb5f5a07d1109aa9
matt-newhall <matt@newhall.dev>
2024-01-02 01:45:28 +0000

    feat: revert svg change for quality

--- apps/frontend/src/App.test.tsx
+++ apps/frontend/src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react"
 import { App } from "./App"
 
-jest.mock("./assets/react.png", () => "./assets/react.png")
+jest.mock("./assets/react.svg", () => "./assets/react.svg")
 
 describe("rendering the home page correctly", () => {
   it("sees the page title when the page renders", () => {
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.png"
+import reactLogo from "./assets/react.svg"
 import { Button } from "./atoms/Button/Button"
 import { Logo } from "./atoms/Logo/Logo"
 
