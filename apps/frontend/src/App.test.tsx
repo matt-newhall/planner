158ee59d2a749985ac2edf617a335320566aa8c9
matt-newhall <matt@newhall.dev>
2024-01-02 01:35:05 +0000

    feat: add basic test to setup jest

---
+++ apps/frontend/src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react"
+import { App } from "./App"
+
+jest.mock("./assets/react.png", () => "./assets/react.png")
+
+describe("rendering the home page correctly", () => {
+  it("sees the page title when the page renders", () => {
+    const screen = render(<App />)
+    expect(screen.getAllByText("LS3 Project with React").length).toStrictEqual(
+      1,
+    )
+  })
+})
